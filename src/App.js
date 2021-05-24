6448134779284e3fd6db7e604f99a74eb34c8426
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-05-24 10:55:38 +0200

    updating App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Header from "./components/Header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/Footer/Footer";
 
-function App() {
-  return <div>Hello from iknow</div>;
+class App extends Component {
+  getRecipe = (event) => {
+    event.preventDefault();
+    console.log("working");
+  };
+  render() {
+    return (
+      <Router>
+        <Container>
+          <Row>
+            <Header />
+          </Row>
+          <Row>
+            <Main />
+          </Row>
+          <Row>
+            <Footer />
+          </Row>
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
