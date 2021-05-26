aaf17f4b24e8c29f54fe544c83866129f3d48b59
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-05-26 20:52:49 +0200

    resolving merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from "react-router-dom";
+
 
 import Header from "./Header/Header";
 import Main from "./Main/Main";
@@ -6,12 +6,7 @@ import Footer from "./Footer/Footer";
 
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import React, { Component } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Header from "./components/Header/Header";
-import Main from "./components/main/Main";
-import Footer from "./components/Footer/Footer";
+
 
 function App() {
   return (
