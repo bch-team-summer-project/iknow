7a84c2b44873c9af22c2ea9759406821ee55ff66
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-05-30 10:35:24 +0200

    adding location icon in cards

--- src/Main/BeachTemp/Beaches.js
+++ src/Main/BeachTemp/Beaches.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from "react";
 import BeachTempratureCard from "./BeachTempratureCard";
 
+
+
 const Beaches = () => {
-  const [beaches, setBeaches] = useState([]);
+  const [state, setState] = useState({
+    beaches:[],
+    cities:[]
+  });
+
+  const cityNames = ["Helsinki", "Espoo", "Vantaa"];
 
   useEffect(() => {
     fetch("https://iknow-backend.herokuapp.com/beachTemp")
       .then((response) => response.json())
       .then((responseJson) => {
-        console.log(responseJson);
-        setBeaches(responseJson);
+        console.log("dfggdfgdfgdfgdfgdfgdfgdfgdfgdfgdfg    dfgdfgdfdfg");
+        setState({...state, beaches: responseJson});
+      
+     
       });
   }, []);
   const renderedResult = (
     <div>
-      {beaches.map((beachTemp) => (
+      {state.beaches.map((beachTemp) => (
         <BeachTempratureCard beachTemprature={beachTemp} />
       ))}
     </div>
