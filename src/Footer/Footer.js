96f5210470649eead2e70a78a56e55aa80e278ea
Yuliia Matvi <jualiasha@gmail.com>
2021-06-07 22:56:15 +0300

    live page changed css

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -22,7 +22,6 @@ const Footer = () => {
     const getData = async () => {
       const res = await axios(`https://iknow-backend.herokuapp.com/weather/`);
       setWeather(res.data);
-      console.log(res.data[0].city);
     };
     getData();
   }, []);
